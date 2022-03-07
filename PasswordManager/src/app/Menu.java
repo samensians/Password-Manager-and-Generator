f392675dd806ccc88557444026b49d4821c7aa00
Samuel Burnhope <s.burnhope11@gmail.com>
2022-03-07 17:28:46 +0000

    Finished main menu and added documentation

--- PasswordManager/src/app/Menu.java
+++ PasswordManager/src/app/Menu.java
@@ -15,9 +15,20 @@ import javax.swing.JSeparator;
 import javax.swing.border.EmptyBorder;
 import javax.swing.SwingUtilities;
 
+/**
+ * Creates a menu that is used to go to either the password generator
+ * or password manager. This is the entry point of the program.
+ * After selecting a menu option, the menu closes and can only be
+ * re-opened by starting the program again.
+ */
 public class Menu extends JFrame {
-    private final int BUTTON_VGAP = 20;
+    private final int BUTTONS_VGAP = 20;
 
+    /**
+     * Create a <code>JFrame</code> to add GUI components to.
+     *
+     * @param windowName a string used to display as the name of the window
+     */
     public Menu(String windowName) {
         super(windowName);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -25,27 +36,39 @@ public class Menu extends JFrame {
         setLocationRelativeTo(null);
     }
 
-    private void addComponentsToPane(final Container pane) {
+    /**
+     * Creates all the components required for the GUI, and adds them to the
+     * pane. Also adds styling and button functionality.
+     *
+     * @param pane the container that the components are added to
+     */
+    private void addComponents(final Container pane) {
         // Create panels and set layouts
         JPanel titlePanel = new JPanel();
         String titleText = "<html><h2>Password Generator and Manager</h2></html>";
         titlePanel.add(new JLabel(titleText, JLabel.CENTER));
 
-        GridLayout buttonLayout = new GridLayout(2, 1);
-        buttonLayout.setVgap(BUTTON_VGAP);
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(buttonLayout);
-        buttonPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+        GridLayout buttonsLayout = new GridLayout(2, 1);
+        buttonsLayout.setVgap(BUTTONS_VGAP);
+        JPanel buttonsPanel = new JPanel();
+        buttonsPanel.setLayout(buttonsLayout);
+        buttonsPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
 
         // Create buttons and add action listeners
         JButton toPasswordGeneratorButton = new JButton("Password Generator");
         toPasswordGeneratorButton.setPreferredSize(new Dimension(150, 50));
         toPasswordGeneratorButton.addActionListener(new ActionListener() {
+            /**
+             * Creates a <code>PasswordGenerator</code> instance and creates
+             * and shows the password generator GUI. Then closes/disposes the menu.
+             *
+             * @param e the event being processed
+             */
             @Override
             public void actionPerformed(ActionEvent e) {
                 SwingUtilities.invokeLater(new Runnable() {
                     public void run() {
-                        new PasswordGenerator("Password Generator").createAndShowGUI();
+                        new app.PasswordGenerator("Password Generator").createAndShowWindow();
                     }
                 });
                 dispose();
@@ -55,11 +78,17 @@ public class Menu extends JFrame {
         JButton toPasswordManagerButton = new JButton("Password Manager");
         toPasswordManagerButton.setPreferredSize(new Dimension(150, 50));
         toPasswordManagerButton.addActionListener(new ActionListener() {
+            /**
+             * Creates a <code>PasswordManager</code> instance and creates
+             * and shows the password manager GUI. Then closes/disposes the menu.
+             *
+             * @param e the event being processed
+             */
             @Override
             public void actionPerformed(ActionEvent e) {
                 SwingUtilities.invokeLater(new Runnable() {
                     public void run() {
-                        // new PasswordManager("Password Manager").createAndShowGUI();
+                        new PasswordManager("Password Manager").createAndShowWindow();
                     }
                 });
                 dispose();
@@ -67,29 +96,35 @@ public class Menu extends JFrame {
         });
         
         // Add the buttons to the panel
-        buttonPanel.add(toPasswordGeneratorButton);
-        buttonPanel.add(toPasswordManagerButton);
+        buttonsPanel.add(toPasswordGeneratorButton);
+        buttonsPanel.add(toPasswordManagerButton);
 
         // Add panels to the pane
         pane.add(titlePanel, BorderLayout.NORTH);
         pane.add(new JSeparator(), BorderLayout.CENTER);
-        pane.add(buttonPanel, BorderLayout.SOUTH);
+        pane.add(buttonsPanel, BorderLayout.SOUTH);
     }
 
-    public static void createAndShowGUI() {
+    /**
+     * Creates an instance of the GUI and adds components, then displays the window.
+     */
+    public static void createAndShowWindow() {
         // Create the menu window 
         Menu menu = new Menu("Password Generator and Manager");
         // Add the content to the pane
-        menu.addComponentsToPane(menu.getContentPane());
+        menu.addComponents(menu.getContentPane());
         // Display the window
         menu.pack();
         menu.setVisible(true);
     }
 
+    /**
+     * Shows the menu when the program is run.
+     */
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                createAndShowGUI();
+                createAndShowWindow();
             }
         });
     }
